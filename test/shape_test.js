c133c8fbc7b648042f01583c143b7728037e117f
David Daniel <davedan7@gmail.com>
2015-07-23 23:49:17 -0600

    refactoring

--- test/shape_test.js
+++ test/shape_test.js
@@ -32,17 +32,57 @@ describe('The Shapes', function() {
   it('can move shapes down', function () {
     var b = new Board();
     var j = new jShape(b);
-    // debugger;
+
     assert(b.isOccupied(5, 0));
     assert(b.isOccupied(5, 1));
     assert(b.isOccupied(5, 2));
     assert(b.isOccupied(4, 2));
+
     j.moveDown();
+
     assert(!b.isOccupied(5, 0));
     assert(b.isOccupied(5, 1));
     assert(b.isOccupied(5, 2));
     assert(b.isOccupied(5, 3));
     assert(b.isOccupied(4, 3));
+  });
+
+  it('can move shapes left', function () {
+    var b = new Board();
+    var j = new jShape(b);
+
+    assert(b.isOccupied(5, 0));
+    assert(b.isOccupied(5, 1));
+    assert(b.isOccupied(5, 2));
+    assert(b.isOccupied(4, 2));
+
+    j.moveLeft();
+
+    assert(!b.isOccupied(5, 0));
+    assert(b.isOccupied(4, 0));
+    assert(b.isOccupied(4, 1));
+    assert(b.isOccupied(4, 2));
+    assert(b.isOccupied(3, 2));
+    
+  });
+
+  it('can move shapes right', function () {
+    var b = new Board();
+    var j = new jShape(b);
+
+    assert(b.isOccupied(5, 0));
+    assert(b.isOccupied(5, 1));
+    assert(b.isOccupied(5, 2));
+    assert(b.isOccupied(4, 2));
+
+    j.moveRight();
+
+    assert(!b.isOccupied(5, 0));
+    assert(b.isOccupied(6, 0));
+    assert(b.isOccupied(6, 1));
+    assert(b.isOccupied(6, 2));
+    assert(b.isOccupied(5, 2));
+    
   });
 
   it('can tell if a piece is locked', function () {
