6572abf156d259da0ff05332928bc0f8d85905d5
David Daniel <davedan7@gmail.com>
2015-07-24 00:12:21 -0600

    Beefed up tests for board

--- test/board_test.js
+++ test/board_test.js
@@ -15,6 +15,16 @@ describe('The board', function() {
     assert.equal(bob.cols, 10);
   });
 
+  it('can make a board of different sizes', function () {
+    var smallBoard = new Board(16, 8);
+    assert.equal(smallBoard.rows, 16);
+    assert.equal(smallBoard.cols, 8);
+
+    var bigBoard = new Board(40, 15);
+    assert.equal(bigBoard.rows, 40);
+    assert.equal(bigBoard.cols, 15);
+  });
+
   it('should store new pieces', function () {
     var board = new Board();
     var piece = new Piece(board);
@@ -28,7 +38,17 @@ describe('The board', function() {
     assert(!board.isOccupied(0, 4));
   });
 
-  it('should know when a row is filled', function () {
+  it('should know if a position is not valid', function () {
+    var board = new Board();
+    var maxValidX = board.cols - 1;
+    var maxValidY = board.rows - 1;
+    assert(!board.isValidPosition(-1, 0));
+    assert(!board.isValidPosition(maxValidX + 1, 0));
+    assert(!board.isValidPosition(0, -1));
+    assert(!board.isValidPosition(0, maxValidY + 1));
+  });
+
+  it('should know when a row is filled and clear it', function () {
     var board = new Board();
     new Piece(board, 0, 19);
     new Piece(board, 1, 19);
@@ -39,10 +59,13 @@ describe('The board', function() {
     new Piece(board, 6, 19);
     new Piece(board, 7, 19);
     new Piece(board, 8, 19);
-    new Piece(board, 9, 19);
 
+    board.checkRows();
     assert(board.isOccupied(1,19));
     assert(board.isOccupied(6,19));
+
+    new Piece(board, 9, 19);
+
     board.checkRows();
     assert(!board.isOccupied(1,19));
     assert(!board.isOccupied(6,19));
